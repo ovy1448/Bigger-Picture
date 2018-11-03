c65736a175e2ab17ac6b99c2a72f9e72248af0e5
Daniel Ovári <dovari@seznam.cz>
2018-11-03 19:15:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+document.getElementById("button").addEventListener("click", loadImages);
+
+function loadImages() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.unsplash.com/search/photos?page=1&query=office&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
+
+    xhr.onload = function() {
+        if(this.status == 200) {
+            var images = JSON.parse(this.responseText);
+
+            var output = "";
+            for(var i in images) {
+                output +=
+                '<div class="image">' +
+                '<img src="'+images[i].urls.small+'">'
+                '</div>';
+            }
+
+            document.getElementById("images").innerHTML = output;
+            console.log(images);
+        }
+
+    }
+    xhr.send();
+}
