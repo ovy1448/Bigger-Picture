4bbd699eb7c588292f140a1773aea8901ec6a279
Daniel Ovári <dovari@seznam.cz>
2018-11-05 14:11:09 +0100

    Search split JS files

--- search.js
+++ search.js
@@ -1,10 +1,14 @@
-function loadImages() {
+document.getElementById("button").addEventListener("click", searchImages);
+
+function searchImages() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.unsplash.com/photos?page=1&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
+    xhr.open("GET", "https://api.unsplash.com/search/photos?page=1&query=sky&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
 
     xhr.onload = function () {
         if (this.status == 200) {
-            var images = JSON.parse(this.responseText);
+            var response = JSON.parse(this.responseText);
+            var images = response.results;
+
 
             var output = "";
             for (var i in images) {
