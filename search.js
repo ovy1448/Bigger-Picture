66c34b44a2c907357e1fba97fc68a637d9da7b3d
Daniel Ovári <dovari@seznam.cz>
2018-11-05 18:22:02 +0100

    Working search

--- search.js
+++ search.js
@@ -1,27 +1,35 @@
-document.getElementById("button").addEventListener("click", searchImages);
+document.getElementById("engine").addEventListener("submit", submitSearch);
 
-function searchImages() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.unsplash.com/search/photos?page=1&query=sky&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
-
-    xhr.onload = function () {
-        if (this.status == 200) {
-            var response = JSON.parse(this.responseText);
-            var images = response.results;
+function submitSearch(e) {
+    var text = document.getElementById("text").value;
+    searchImage(text);
 
 
-            var output = "";
-            for (var i in images) {
-                output +=
-                    '<div class="image">' +
-                    '<img src="' + images[i].urls.small + '">'
-                '</div>';
+    function searchImage(text) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://api.unsplash.com/search/photos?page=1&query='" + text + "'&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
+
+        xhr.onload = function () {
+            if (this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                var images = response.results;
+
+
+                var output = "";
+                for (var i in images) {
+                    output +=
+                        '<div class="image">' +
+                        '<img src="' + images[i].urls.small + '">'
+                    '</div>';
+                }
+
+                document.getElementById("images").innerHTML = output;
+                console.log(images);
             }
 
-            document.getElementById("images").innerHTML = output;
-            console.log(images);
         }
+        xhr.send();
 
     }
-    xhr.send();
+    e.preventDefault();
 }
