05b16d12607118f52c379636c661420e28c17e6a
Daniel Ovári <dovari@seznam.cz>
2018-11-06 18:57:46 +0100

    Both next page btns working / preventDefault err

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+document.getElementById("nextRandom").addEventListener("click", nextRandom);
+
 function loadImages() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.unsplash.com/photos/random?count=10&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
+    xhr.open("GET", "https://api.unsplash.com/photos/random?count=10&page=" + s + "&client_id=15020f1f31839a088aff745486e7a469cd064761ff165c9d3d9f57de77d10348", true);
 
     xhr.onload = function () {
         if (this.status == 200) {
@@ -20,5 +22,11 @@ function loadImages() {
 
     }
     xhr.send();
+    console.log(s);
 };
 
+var s = 1;
+function nextRandom() {
+    s += 1;
+    return loadImages();
+};
--- search.js
+++ search.js
@@ -8,6 +8,7 @@ function submitSearch(e) {
     } else {
         searchImage(text);
         document.getElementById("nextPage").style.display = "block";
+        document.getElementById("nextRandom").style.display = "none";
     };
     e.preventDefault();
 }
@@ -34,6 +35,7 @@ function searchImage(text) {
     }
 
     xhr.send();
+    console.log(a);
 }
 
 var a = 1;
